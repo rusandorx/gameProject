1912cfcf85dc7e73755c44d4d90daeb89acdf6ab
Rusandor <ruslanpalkin44@gmail.com>
2023-01-12 21:24:41 +0300

    Map fixed

---
+++ map/game_map/TX Player.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="TX Player" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="tilemaps/TX Player.png" width="128" height="128"/>
+</tileset>
