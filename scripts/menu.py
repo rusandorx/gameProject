1ef3bb525c83a164987b8068073dbbcbf1ac6e7e
Rusandor <ruslanpalkin44@gmail.com>
2023-01-13 21:49:58 +0300

    Map

--- scripts/menu.py
+++ scripts/menu.py
@@ -7,6 +7,7 @@ from settings import *
 class Menu():
     def __init__(self, game):
         self.game = game
+        self.game_state = 'menu'
         self.mid_w, self.mid_h = self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2
         self.run_display = True
         self.cursor_rect = pygame.Rect(0, 0, 20, 20)
