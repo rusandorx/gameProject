f9db35262126294e0940fa234fcd762e18eca205
Almaz <3112almaz@gmail.com>
2023-01-13 21:49:14 +0300

    Экран

--- scripts/main.py
+++ scripts/main.py
@@ -5,6 +5,36 @@ from settings import *
 from level import Level
 
 
+def start_screen():
+    size = width, height = 1280, 720
+    screen = pygame.display.set_mode(size)
+    intro_text = []
+    clock = pygame.time.Clock()
+
+    fon = pygame.transform.scale(pygame.image.load('../graphics/menu/images.jpg'), (1280, 720))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
+
 class Game:
     def __init__(self):
         pygame.init()
@@ -28,5 +58,6 @@ class Game:
 
 
 if __name__ == '__main__':
+    start_screen()
     game = Game()
     game.run()
