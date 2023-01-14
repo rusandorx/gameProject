d812877ad6df684f7d9bfb54a42bf749e721d694
Almaz <3112almaz@gmail.com>
2023-01-14 22:25:48 +0300

    Звуки доработанные

--- scripts/sounds.py
+++ scripts/sounds.py
@@ -2,7 +2,13 @@ import pygame
 
 
 class Sound:
-    def __init__(self, name, time_fadeout):
-        s = pygame.mixer.Sound(name)
-        s.play()
-        pygame.mixer.music.fadeout(time_fadeout)
+    def __init__(self, name, time):
+        self.sound = pygame.mixer.Sound(name)
+        self.sound.play(time)
+        self.time = time
+
+    def stop(self):
+        self.sound.stop()
+
+    def play(self):
+        self.sound.play(self.time)
